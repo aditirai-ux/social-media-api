2a59e71f1e03e9849032de79925357b4171039b5
Aditi Rai <aditi45@gmail.com>
2024-10-28 18:42:35 +0530

    created reaction model

---
+++ src/models/Reaction.ts
@@ -0,0 +1,38 @@
+import e from 'express';
+import { Schema, Document, ObjectId, Types } from 'mongoose';
+
+interface IReaction extends Document {
+    reactionId: ObjectId;
+    reactionBody: string;
+    username: string;
+    createdAt: Date;
+}
+
+const reactionSchema = new Schema<IReaction>({
+    reactionId: {
+        type: Types.ObjectId,
+        default: new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal: Date) => createdAtVal.toDateString()
+    }
+},
+{
+    toJSON: {
+        getters: true
+    },
+    id: false
+});
+
+export default reactionSchema;

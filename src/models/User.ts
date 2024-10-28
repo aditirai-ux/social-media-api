095a0da9b7e3957d86a6ca6e5af813f7f16d2e28
Aditi Rai <aditi45@gmail.com>
2024-10-28 18:01:16 +0530

    updated user model

---
+++ src/models/User.ts
@@ -0,0 +1,45 @@
+import { Schema, Document, model, ObjectId } from 'mongoose';
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  thoughts: ObjectId[];
+  friends: ObjectId[];
+}
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+});
+// Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});

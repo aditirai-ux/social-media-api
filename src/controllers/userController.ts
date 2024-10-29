7e27fea3babdf77226ba548a9ea5d89021b0a35a
Aditi Rai <aditi45@gmail.com>
2024-10-30 00:55:02 +0530

    creater getuser finction in userController file

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,24 @@
-export const getUsers();
+import { Request, Response } from 'express';
+import { User, Thought } from '../models/index';
+
+// Get all users
+export const getUsers = async (req: Request, res: Response) => {
+    try {
+        const userData = await User.find({});
+        res.json(userData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+// Get single user by _id
 export const getSingleUser();
+// Create a user
 export const createUser();
+// Update user by _id
 export const updateUser();
+// Delete user by _id
 export const deleteUser();
+// Add a friend to a user's friend list using friendID
 export const addFriend(); 
+// Remove a friend from a user's friend list using friendID
 export const removeFriend();

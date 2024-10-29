ef0af0fca4d74af9872354fb5b102965bf659394
Aditi Rai <aditi45@gmail.com>
2024-10-30 01:10:58 +0530

    completed user functions in userController file

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { User, Thought } from '../models/index';
 
 // Get all users
-export const getUsers = async (req: Request, res: Response) => {
+export const getUsers = async (_req: Request, res: Response) => {
     try {
         const userData = await User.find({});
         res.json(userData);
@@ -13,18 +13,60 @@ export const getUsers = async (req: Request, res: Response) => {
 // Get single user by _id
 export const getSingleUser = async (req: Request, res: Response) => {
     try {
-        const userData = await User.findOne({ _id: req.params.userId });
+        const userData = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+        if (!userData) {
+            return res.status(404).json({ message: 'No user found with this id!' });
+        }  
+        res.json(userData);
+        return;
+    } catch (err) {
+        res.status(400).json(err);
+        return;
+    }
+}
+// Create a user
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const userData = await User.create(req.body);
         res.json(userData);
     } catch (err) {
         res.status(400).json(err);
     }
 }
-// Create a user
-export const createUser();
 // Update user by _id
-export const updateUser();
-// Delete user by _id
-export const deleteUser();
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+        const userData = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { new: true, runValidators: true }
+        );
+        if (!userData) {
+            return res.status(404).json({ message: 'No user found with this id!' });
+        }
+        res.json(userData);
+        return;
+    } catch (err) {
+        res.status(400).json(err);
+        return;
+    }
+}
+// Delete user by _id and associated thoughts
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const userData = await User.findOneAndDelete({ _id: req.params.userId });
+        if (!userData) {
+            return res.status(404).json({ message: 'No user found with this id!' });
+        }
+        await Thought.deleteMany({ _id: { $in: userData.thoughts } });
+        res.json({ message: 'User and associated thoughts deleted!' });
+        return;
+    } catch (err) {
+        res.status(400).json(err);
+        return;
+    }
+}
 // Add a friend to a user's friend list using friendID
 export const addFriend(); 
 // Remove a friend from a user's friend list using friendID
